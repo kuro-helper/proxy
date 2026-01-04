f17a9d9128166c36288175ca85dac905fd38fdc1
SeaotterMS <peter910820@gmail.com>
2026-01-04 17:28:32 +0800

    chore: unified proxy feature func

--- proxy.go
+++ proxy.go
@@ -2,9 +2,7 @@ package proxy
 
 import (
 	"fmt"
-	"io"
 	"net"
-	"net/http"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -15,19 +13,24 @@ var (
 	dialer proxy.Dialer
 )
 
-// 取得proxy
-//
-// 使用VPN版本
-func GetProxyDialerVPN(authUser, authPwd, proxyAddr, proxyPort string) (proxy.Dialer, error) {
+// 取得proxy dialer
+func GetProxyDialer(proxyAddr string, proxyAuth *proxy.Auth, proxyPort string) (proxy.Dialer, error) {
 	if dialer == nil {
 		proxyAddr := fmt.Sprintf("%s:%s", proxyAddr, proxyPort)
-		auth := proxy.Auth{User: authUser, Password: authPwd}
 
 		var err error
-		dialer, err = proxy.SOCKS5("tcp", proxyAddr, &auth, &net.Dialer{
-			Timeout: 10 * time.Second,
-		})
+		if proxyAuth != nil {
+			dialer, err = proxy.SOCKS5("tcp", proxyAddr, proxyAuth, &net.Dialer{
+				Timeout: 10 * time.Second,
+			})
+		} else {
+			dialer, err = proxy.SOCKS5("tcp", proxyAddr, nil, &net.Dialer{
+				Timeout: 10 * time.Second,
+			})
+		}
+
 		if err != nil {
+			dialer = nil
 			return nil, fmt.Errorf("%w: %v", ErrCreateSOCKS5DialerFailed, err)
 		}
 		logrus.Debugf("%s Proxy已成功設置", proxyAddr)
@@ -35,57 +38,6 @@ func GetProxyDialerVPN(authUser, authPwd, proxyAddr, proxyPort string) (proxy.Di
 	return dialer, nil
 }
 
-// 取得proxy
-//
-// 私有配置版本
-func GetProxyDialer(apiURL, apiKey, proxyPort string) (proxy.Dialer, error) {
-	if dialer == nil {
-		// 1. 從 API 獲取當前 Proxy IP
-		proxyIP, err := getProxyIP(apiURL, apiKey)
-		if err != nil {
-			return nil, fmt.Errorf("%w: %v", ErrGetProxyIPFailed, err)
-		}
-		logrus.Debugf("當前 Proxy IP: %s\n", proxyIP)
-
-		// 2. 建立 SOCKS5 Dialer
-		proxyAddr := fmt.Sprintf("%s:%s", proxyIP, proxyPort)
-
-		// 3. 創建 SOCKS5 dialer
-		dialer, err = proxy.SOCKS5("tcp", proxyAddr, nil, proxy.Direct)
-		if err != nil {
-			return nil, fmt.Errorf("%w: %v", ErrCreateSOCKS5DialerFailed, err)
-		}
-	}
-	return dialer, nil
-}
-
-// 呼叫管理 API 獲取 IP
-//
-// 私有配置版本使用
-func getProxyIP(apiURL, apiKey string) (string, error) {
-	client := &http.Client{Timeout: 5 * time.Second}
-
-	req, err := http.NewRequest("GET", apiURL, nil)
-	if err != nil {
-		return "", fmt.Errorf("%w: %v", ErrCreateRequestFailed, err)
-	}
-
-	req.Header.Set("X-Allowed-Key", apiKey)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", fmt.Errorf("%w: %v", ErrHTTPRequestFailed, err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return "", fmt.Errorf("%w: %d", ErrAPIStatusCodeError, resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", fmt.Errorf("%w: %v", ErrReadResponseFailed, err)
-	}
-
-	return string(body), nil
+func GenerateProxyAuth(authUser, authPwd string) *proxy.Auth {
+	return &proxy.Auth{User: authUser, Password: authPwd}
 }
